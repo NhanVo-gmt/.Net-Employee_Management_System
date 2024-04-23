7f7b6c1a405aa82204700664cd936652ce62c7c1
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-04-23 17:14:03 +1000

    user account

---
+++ ServerLibray/Helpers/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerLibray.Helpers
+{
+    public class Constants
+    {
+        public static string Admin { get; set; } = "Admin";
+        public static string User { get; set; } = "User";
+    }
+}
--- ServerLibray/Repositories/Implementations/UserAccountRepository.cs
+++ ServerLibray/Repositories/Implementations/UserAccountRepository.cs
@@ -1,5 +1,10 @@
 ﻿using BaseLibrary.DTOs;
+using BaseLibrary.Entities;
 using BaseLibrary.Responses;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using ServerLibray.Data;
+using ServerLibray.Helpers;
 using ServerLibray.Repositories.Contracts;
 using System;
 using System.Collections.Generic;
@@ -9,11 +14,57 @@ using System.Threading.Tasks;
 
 namespace ServerLibray.Repositories.Implementations
 {
-    public class UserAccountRepository : IUserAccount
+    public class UserAccountRepository(IOptions<JwtSection> config, AppDbContext context) : IUserAccount
     {
-        public Task<GeneralResponse> CreateAsync(Register user)
+        public async Task<GeneralResponse> CreateAsync(Register user)
         {
-            throw new NotImplementedException();
+            if (user is null) return new GeneralResponse(false, "Model is empty");
+
+            var checkUser = await FindUserByEmail(user.Email!);
+            if (checkUser != null) return new GeneralResponse(false, "User already registered");
+
+            // Save User 
+            var applicationUser = await AddToDatabase(new ApplicationUser()
+            {
+                FullName = user.FullName,
+                Email = user.Email,
+                Password = BCrypt.Net.BCrypt.HashPassword(user.Password)
+            });
+
+            // Check and assign rules
+            var checkAdminRole = await context.SystemRoles.FirstOrDefaultAsync(_ => _.Name!.Equals(Constants.Admin));
+            if (checkAdminRole is null)
+            {
+                var createAdminRole = await AddToDatabase(new SystemRole() { Name = Constants.Admin });
+                await AddToDatabase(new UserRole() { RoleId = createAdminRole.Id, UserID = applicationUser.Id });
+                return new GeneralResponse(true, "Accounts created !");
+            }
+
+            var checkUserRole = await context.SystemRoles.FirstOrDefaultAsync(_ => _.Name!.Equals(Constants.User));
+            SystemRole response = new SystemRole();
+            if (checkUserRole is null)
+            {
+                response = await AddToDatabase(new SystemRole() { Name = Constants.User });
+                await AddToDatabase(new UserRole() { RoleId = response.Id, UserID = applicationUser.Id });
+            }
+            else
+            {
+                await AddToDatabase(new UserRole() { RoleId = checkUserRole.Id, UserID = applicationUser.Id });
+
+            }
+            return new GeneralResponse(true, "Accounts created !");
+        }
+
+        private async Task<T> AddToDatabase<T>(T model)
+        {
+            var result = context.Add(model!);
+            await context.SaveChangesAsync();
+            return (T)result.Entity;
+        }
+
+        private async Task<ApplicationUser> FindUserByEmail(string email)
+        {
+            return await context.ApplicationUsers.FirstOrDefaultAsync(_ => _.Email!.ToLower()!.Equals(email!.ToLower()));
         }
 
         public Task<LoginResponse> SignInAsync(Login user)

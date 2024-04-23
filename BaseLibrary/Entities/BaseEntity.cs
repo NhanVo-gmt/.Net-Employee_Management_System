8449f0ecf22a21c4d27175471ce7d1e1de2a90cc
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-04-23 14:49:24 +1000

    adding employee information

--- BaseLibrary/Entities/BaseEntity.cs
+++ BaseLibrary/Entities/BaseEntity.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BaseLibrary.Entities
+﻿namespace BaseLibrary.Entities
 {
     public class BaseEntity
     {
         public int Id { get; set; }
         public string? Name { get; set; }
 
-        // Relationship : One to Many
-        public List<Employee> Employees { get; set; }
+        
     }
 }
